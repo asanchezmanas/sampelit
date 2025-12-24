911df3411fcf72cf88173d9821467943551eaf60
asanchezmanas <157375517+asanchezmanas@users.noreply.github.com>
2025-12-24 11:21:17 +0100

    Update __init__.py

--- orchestration/services/segmentation/v3/__init__.py
+++ orchestration/services/segmentation/v3/__init__.py
@@ -19,3 +19,24 @@ __all__ = [
     'AdaptiveContextualAllocator',
     'SegmentAnalyzer'
 ]
+
+"""
+Hierarchical Clustering Module (V3.2)
+
+Multi-level segmentation with cascade allocation.
+
+Copyright (c) 2024 Samplit Technologies
+"""
+
+__version__ = "3.2.0"
+
+from .hierarchy_builder import HierarchyBuilder, SegmentNode
+from .cascade_allocator import CascadeAllocator
+from .tree_visualizer import TreeVisualizer
+
+__all__ = [
+    'HierarchyBuilder',
+    'SegmentNode',
+    'CascadeAllocator',
+    'TreeVisualizer'
+]
